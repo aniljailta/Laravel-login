a7761c8944242f74675ce268c151f5c53485ca8c
aniljailta <aniljailta@gmail.com>
2015-02-22 03:14:16 +0530

    migrations and seed

---
+++ app/database/migrations/2015_02_18_191355_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+
+		 Schema::create('logs', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('ip');
+			$table->string('location');
+			$table->string('login_attempt');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('logs');
+	}
+
+}
