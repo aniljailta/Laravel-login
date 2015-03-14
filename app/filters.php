126d7be8d83a11d8e46ba211da0c008119fd65f4
aniljailta <aniljailta@gmail.com>
2015-03-14 17:50:21 +0530

    Filter update

--- app/filters.php
+++ app/filters.php
@@ -80,7 +80,6 @@ Route::filter('guest', function()
 | session does not match the one given in this request, we'll bail.
 |
 */
-
 Route::filter('csrf', function()
 {
 	if (Session::token() != Input::get('_token'))
